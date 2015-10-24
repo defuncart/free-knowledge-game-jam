6082f70393d2f2ee755e1e614610b0d60c682fa5
defuncart <defuncart@gmail.com>
2015-10-24 19:08:06 +0200

    Initial commit

--- js/GameEngine.js
+++ js/GameEngine.js
@@ -5,7 +5,7 @@ function calculatePlayerSupport()
 	var temp;
 	for(i=0; i < counties.length; i++)
 	{
-		temp += counties[i].playerSupport
+		temp += (counties[i].playerSupport*1.0)
 	}
 	console.log( "counties.length: " + counties.length )
 	playerSupport = temp / counties.length
@@ -14,14 +14,14 @@ function calculatePlayerSupport()
 var government = Government();
 
 var counties = [];
-counties.push( new County("capital", 0, 100, 65, 0.13) );
-counties.push( new County("universiy", 0, 80, 50, 0.09) );
-counties.push( new County("business", 0, 80, 25, 0.07) );
-counties.push( new County("township1", 0, 50, 30, 0.15) );
-counties.push( new County("township2", 0, 40, 15, 0.17) );
-counties.push( new County("countryside1", 0, 20, 40, 0.15) );
-counties.push( new County("countryside2", 0, 20, 70, 0.12) );
-counties.push( new County("countryside3", 0, 10, 10, 0.12) );
+counties.push( new County("capital", 2, 100, 65, 0.13) );
+counties.push( new County("universiy", 1, 80, 50, 0.09) );
+counties.push( new County("business", 7, 80, 25, 0.07) );
+counties.push( new County("township1", 1, 50, 30, 0.15) );
+counties.push( new County("township2", 2, 40, 15, 0.17) );
+counties.push( new County("countryside1", 1, 20, 40, 0.15) );
+counties.push( new County("countryside2", 2, 20, 70, 0.12) );
+counties.push( new County("countryside3", 3, 10, 10, 0.12) );
 
 function GameEngine()
 {
