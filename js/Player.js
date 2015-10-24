4ba4acae67b173b0baeee7d9c77ea0205f573337
defuncart <defuncart@gmail.com>
2015-10-24 21:54:12 +0200

    Initial Commit

--- js/Player.js
+++ js/Player.js
@@ -2,8 +2,6 @@ function Player()
 {
 	console.log("Player")
 	this.actions = [];
-<<<<<<< HEAD
-	//support needed, support gain, recruit chance, catch chance, global, unlocked
 	this.actions.push( new SocialMedia(0, 1, 0.2, 0.1, true, true) );
 	this.actions.push( new Graffiti(0, 1, 0.2, 0.2, false, true) );
 	this.actions.push( new Speaking(0, 4, 0.25, 0.2, false, true) );
@@ -13,14 +11,4 @@ function Player()
 	this.actions.push( new Newspapers(15, 15, 0.50, 0.4, false, false) );
 	this.actions.push( new Hacking(25, 18, 0.40, 0.5, true, false) );
 	this.recruits = [];
-=======
-	this.actions.push( new SocialMedia(0, 1, 1, 0.1, true, true) );
-	this.actions.push( new Graffiti(0, 1, 1, 0.1, true, true) );
-	this.actions.push( new Speaking(0, 1, 1, 0.1, true, true) );
-	this.actions.push( new Flyers(0, 1, 1, 0.1, true, true) );
-	this.actions.push( new Demo(0, 1, 1, 0.1, true, true) );
-	this.actions.push( new Election(0, 1, 1, 0.1, true, true) );
-	this.actions.push( new Newspapers(0, 1, 1, 0.1, true, false) );
-	this.actions.push( new Hacking(0, 1, 1, 0.1, true, false) );
->>>>>>> afa6e1d0eae10ccab026e708f22b44a66a97b7be
 }
