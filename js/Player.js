c51c161ceeebc7d6bc66e722af3a90d8e95891fd
defuncart <defuncart@gmail.com>
2015-10-24 16:52:44 +0200

    Initial commit

--- js/Player.js
+++ js/Player.js
@@ -2,5 +2,5 @@ function Player()
 {
 	this.support = 0;
 	this.actions = [];
-	this.actions.push( new SocailMedia(0, 1, 1, 0.1, true, 2) );
+	this.actions.push( new SocialMedia(0, 1, 1, 0.1, true, 2) );
 }
